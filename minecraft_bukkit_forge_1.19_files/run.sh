faa23265d0da8fd3c215a78437558b50ad31d0d0
Maxwell Moord <maxwellmoord@gmail.com>
2022-07-18 17:20:07 -0400

    Renamed the forge file, added the bukkit files, transferred all the world/player data to the bukkit folder

---
+++ minecraft_bukkit_forge_1.19_files/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# Forge requires a configured set of both JVM and program arguments.
+# Add custom JVM arguments to the user_jvm_args.txt
+# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
+#  pass them to this script directly
+java -Xmx48G -Xms45 -jar craftbukkit-1.19.jar
